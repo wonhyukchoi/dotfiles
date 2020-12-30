eff2aa9cb3f121876865864b92b6db14757d6ba4
Wonhyuk Choi <52129690+wonhyukchoi@users.noreply.github.com>
2020-12-30 16:46:21 +0900

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -6,6 +6,7 @@ sudo pacman -S vim
 sudo pacman -S docker
 sudo pacman -S nodejs
 sudo pacman -S dos2unix
+sudo pacman -S latexlive-most
 
 python3 -m pip install -U pip
 python3 -m pip install numpy pandas matplotlib ipython
