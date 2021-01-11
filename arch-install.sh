0fd669f246d61f65537f072fea221a25b1f0e2f0
Wonhyuk Choi <52129690+wonhyukchoi@users.noreply.github.com>
2021-01-11 10:24:52 +0900

    Add wakatime & vundle

--- arch-install.sh
+++ arch-install.sh
@@ -15,7 +15,12 @@ python3 -m pip install jupyter notebook
 sudo snap install code --classic
 sudo snap install sublime-text --classic
 sudo snap install intellij-idea-ultimate --classic
+sudo snap install pycharm-professional --classic
+sudo snap install clion --classic
 sudo snap install spotify
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo "Plugin 'wakatime/vim-wakatime'" >> ~/.vimrc && vim +PluginInstall
 
 sudo pacman -S --needed base-devel git
 git clone https://aur.archlinux.org/yay-git.git
